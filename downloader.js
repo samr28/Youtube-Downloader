469f0089716edb460fa2398a0fd1938273d1ba2b
Sam Roquitte <samroq@yahoo.com>
2018-07-06 23:46:42 -0500

    Add support for creating dirs

--- downloader.js
+++ downloader.js
@@ -2,18 +2,22 @@ const { exec } = require("child_process");
 
 module.exports = {
   download: function(data) {
-    let cmd = "cd " + process.env.DOWNLOAD_LOCATION + "; ";
+    let cmd = `cd ${process.env.DOWNLOAD_LOCATION}; `;
+    if (data.artist) {
+      // Have artist, make dir and cd in
+      cmd += `if [ ! -d '${data.artist}' ]; then mkdir '${data.artist}'; fi; `;
+      cmd += `cd '${data.artist}'; `;
+    }
+    if (data.album) {
+      // Have album, make dir and cd in
+      cmd += `if [ ! -d '${data.album}' ]; then mkdir '${data.album}'; fi; `;
+      cmd += `cd '${data.album}'; `;
+    }
     cmd +=
       "youtube-dl -x --audio-format mp3 -o '" +
-      data.artist +
-      " - " +
-      data.album +
-      " - " +
       data.title +
       ".%(ext)s' " +
       data.url;
-
-    console.log(cmd);
 
     let promise = new Promise(function(resolve, reject) {
       exec(cmd, (err, stdout, stderr) => {
