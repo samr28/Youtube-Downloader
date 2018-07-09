c1b3c3947df25ea8b41135467d9d8dc47c45bb7d
Sam Roquitte <samroq@yahoo.com>
2018-07-08 20:09:40 -0500

    Add metadata to mp3

--- downloader.js
+++ downloader.js
@@ -2,25 +2,11 @@ const { exec } = require("child_process");
 
 module.exports = {
   download: function(data) {
-    let cmd = `cd ${process.env.DOWNLOAD_LOCATION}; `;
-    if (data.artist) {
-      // Have artist, make dir and cd in
-      cmd += `if [ ! -d '${data.artist}' ]; then mkdir '${data.artist}'; fi; `;
-      cmd += `cd '${data.artist}'; `;
-    }
-    if (data.album) {
-      // Have album, make dir and cd in
-      cmd += `if [ ! -d '${data.album}' ]; then mkdir '${data.album}'; fi; `;
-      cmd += `cd '${data.album}'; `;
-    }
-    cmd +=
-      "youtube-dl -x --audio-format mp3 -o '" +
-      data.title +
-      ".%(ext)s' " +
-      data.url;
-
+    let downloadCmd = `./download ${data.url} ${data.title} ${data.artist} ${
+      data.album
+    }`;
     let promise = new Promise(function(resolve, reject) {
-      exec(cmd, (err, stdout, stderr) => {
+      exec(downloadCmd, (err, stdout, stderr) => {
         if (err) {
           reject(err);
         } else if (stderr) {
