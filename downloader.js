57ef3f447440a4f6374244f6542e8cddfc5f12a1
Sam Roquitte <samroq@yahoo.com>
2018-08-05 20:22:34 -0500

    Downloader fixes

--- downloader.js
+++ downloader.js
@@ -2,7 +2,8 @@ const { exec } = require("child_process");
 
 module.exports = {
   download: function(data) {
-    let downloadCmd = 
+    console.log(`Download started`, data);
+    let downloadCmd =
     `./download "${data.url}" "${data.title}" "${data.artist}" "${data.album}"`;
     let promise = new Promise(function(resolve, reject) {
       exec(downloadCmd, (err, stdout, stderr) => {
