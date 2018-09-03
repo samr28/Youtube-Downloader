3eb2772e592fc6b8bf5e7c2e5c1f8a6de9a2c2d8
samr28 <samroq@yahoo.com>
2018-09-03 18:17:28 -0500

    Add autofill options

--- gettitle.js
+++ gettitle.js
@@ -1,22 +1,18 @@
 const { exec } = require("child_process");
 
 module.exports = {
-  download: function(data) {
-    // Escape quotes
-    data.title = data.title.replace(/\"/g, '\\"');
-    data.album = data.album.replace(/\"/g, '\\"');
-    data.artist = data.artist.replace(/\"/g, '\\"');
-    console.log(`Download started`, data);
+  getTitle: function(data) {
+    console.log(`Get title`, data);
     let downloadCmd =
-    `./download "${data.url}" "${data.title}" "${data.artist}" "${data.album}"`;
+    `./gettitle "${data.url}"`;
     let promise = new Promise(function(resolve, reject) {
       exec(downloadCmd, (err, stdout, stderr) => {
         if (err) {
-          console.log(`Downloader error: ${err}`);
+          console.log(`gettitle error: `);
           console.log(err);
           reject(err);
         } else if (stderr) {
-          console.log(`Downloader stderror: ${stderr}`);
+          console.log(`gettitle stderror: `);
           console.log(stderr);
           reject(stderr);
         } else {
--- run.js
+++ run.js
@@ -12,6 +12,7 @@ var spotify = new Spotify({
 });
 
 var downloader = require("./downloader.js");
+var gettitle = require("./gettitle.js");
 var songlist = require("./songList.js");
 
 var version = require("./package.json").version;
@@ -41,45 +42,99 @@ io.on("connection", function(socket) {
       } else {
         socket.emit("msg", "info", "Download started");
         downloader
-          .download(data)
-          .then(function(stdout) {
-            socket.emit("msg", "success", "Download complete");
-            socket.emit("resetform");
-            songlist.getSongs().then(function (songs) {
-              socket.emit("songlist", constructTable(songs));
-            })
-            .catch(function(err) {
-              socket.emit("msg", "error", err);
-            });
+        .download(data)
+        .then(function(stdout) {
+          socket.emit("msg", "success", "Download complete");
+          socket.emit("resetform");
+          songlist.getSongs().then(function (songs) {
+            socket.emit("songlist", constructSongListTable(songs));
           })
           .catch(function(err) {
-            socket.emit("msg", "error", err.toString());
+            socket.emit("msg", "error", err);
           });
+        })
+        .catch(function(err) {
+          socket.emit("msg", "error", err.toString());
+        });
       }
     } else {
       socket.emit("msg", "error", "Incorrect password");
     }
   });
+
+  // Fill songlist
   songlist.getSongs().then(function (songs) {
-    socket.emit("songlist", constructTable(songs));
+    socket.emit("songlist", constructSongListTable(songs));
   })
   .catch(function(err) {
     socket.emit("msg", "error", err);
   });
-  socket.on("autocomplete", function(data) {
+
+  // Autocomplete based on title
+  socket.on("autocomplete-title", function(data) {
     if (data.title) {
       spotify.search({
         type: 'track',
         query: data.title
       }, function(err, data) {
         if (err) {
-          return console.log(err);
+          socket.emit("msg", "error", "Spotify API Error!");
+          return console.log("Spotify API ERROR: ", err);
         }
         let suggestion = {};
         suggestion.title = data.tracks.items[0].name;
         suggestion.artist = data.tracks.items[0].artists[0].name;
-        suggestion.album = data.tracks.items[0].album.name
+        suggestion.album = data.tracks.items[0].album.name;
         socket.emit("complete", suggestion);
+      });
+    }
+  });
+
+  // Autocomplete based on utl
+  socket.on("autocomplete-url", function(data) {
+    if (data.url) {
+      gettitle
+      .getTitle(data)
+      .then(function(stdout) {
+        // Remove junk from title
+        let title = stdout.replace(/[\(\[]official audio[\)\]]|[\(\[]official video[\)\]]|[\(\[]audio[\)\]]|[\(\[]video[\)\]]|[\(\[]monstercat release[\)\]]|[\(\[]official[\)\]]/gi, "");
+        // Search spotify for the track from the url
+        spotify.search({
+          type: 'album,artist,track',
+          query: title
+        }, function(err, spotifyData) {
+          if (err) {
+            return console.log(err);
+          }
+          let suggestions = [];
+          if (!spotifyData || !spotifyData.tracks || !spotifyData.tracks.items || !spotifyData.tracks.items[0]) {
+            socket.emit("msg", "warning", "No match");
+            socket.emit("nomatch");
+            return;
+          }
+          let tracks = spotifyData.tracks.items;
+
+          // Top suggestion
+          let suggestion = {
+            title: tracks[0].name,
+            artist: tracks[0].artists[0].name,
+            album: tracks[0].album.name
+          };
+          socket.emit("complete", suggestion);
+
+          // Other suggestions
+          for (let i = 0; i < tracks.length; i++) {
+            suggestions[i] = {
+              title: tracks[i].name,
+              artist: tracks[i].artists[0].name,
+              album: tracks[i].album.name
+            };
+          }
+          socket.emit("suggestions", constructSuggestionsTable(suggestions), suggestions);
+        });
+      })
+      .catch(function(err) {
+        socket.emit("msg", "error", err.toString());
       });
     }
   });
@@ -90,36 +145,77 @@ http.listen(process.env.WEB_PORT, function() {
   console.log(`listening on *:${process.env.WEB_PORT}`);
 });
 
+function constructSuggestionsTable(suggestions) {
+  let tableHTML = `
+  <table id="dtSuggestions" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+  <thead>
+  <tr>
+  <th class="th-sm">
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  <th class="th-sm">Title
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  <th class="th-sm">Artist
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  <th class="th-sm">Album
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  </tr>
+  </thead>
+  <tbody>
+  `;
+  for (let i = 0; i < suggestions.length; i++) {
+    tableHTML += `
+    <tr>
+    <td>
+      <button class="btn btn-sm center blue-gradient" onclick="pickSuggestion(${i})">
+        <i class="fas fa-chevron-up"></i>
+      </button>
+    </td>
+    <td>${suggestions[i].title || ''}</td>
+    <td>${suggestions[i].artist || ''}</td>
+    <td>${suggestions[i].album || ''}</td>
+    </tr>
+    `;
+  }
+  tableHTML += `
+  </tbody>
+  </table>
+  `;
+  return tableHTML;
+}
 
-function constructTable(songs) {
+function constructSongListTable(songs) {
   let tableHTML = `
   <table id="dtSongList" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-    <thead>
-      <tr>
-        <th class="th-sm">Title
-          <i class="fa fa-sort float-right" aria-hidden="true"></i>
-        </th>
-        <th class="th-sm">Artist
-          <i class="fa fa-sort float-right" aria-hidden="true"></i>
-        </th>
-        <th class="th-sm">Album
-          <i class="fa fa-sort float-right" aria-hidden="true"></i>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
+  <thead>
+  <tr>
+  <th class="th-sm">Title
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  <th class="th-sm">Artist
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  <th class="th-sm">Album
+  <i class="fa fa-sort float-right" aria-hidden="true"></i>
+  </th>
+  </tr>
+  </thead>
+  <tbody>
+  `;
+  for (let i = 0; i < songs.length; i++) {
+    tableHTML += `
+    <tr>
+    <td>${songs[i].title || ''}</td>
+    <td>${songs[i].artist || ''}</td>
+    <td>${songs[i].album || ''}</td>
+    </tr>
     `;
-    for (let i = 0; i < songs.length; i++) {
-      tableHTML += `
-        <tr>
-          <td>${songs[i].title || ''}</td>
-          <td>${songs[i].artist || ''}</td>
-          <td>${songs[i].album || ''}</td>
-        </tr>
-      `;
-    }
+  }
   tableHTML += `
-    </tbody>
+  </tbody>
   </table>
   `;
   return tableHTML;
